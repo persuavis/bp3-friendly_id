fa0578655fc4b6fa4b137c6f3dec34fe008c782b
Wim den Braven <wimdenbraven@persuavis.com>
2024-01-08 15:28:22 -0800

    Initial commit

---
+++ lib/bp3/friendly_id.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'friendly_id/version'
+
+module Bp3
+  module FriendlyId
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/bp3/friendly_id/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Bp3
+  module FriendlyId
+    VERSION = '0.1.0'
+  end
+end
